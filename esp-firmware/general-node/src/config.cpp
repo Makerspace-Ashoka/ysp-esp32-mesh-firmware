6c11c20bad4f3023ffb8d5792d59f34c026341f4
Sai Khurana <44598300+saikhurana98@users.noreply.github.com>
2024-06-03 17:47:22 +0530

    add config constructor

---
+++ esp-firmware/general-node/src/config.cpp
@@ -0,0 +1,20 @@
+#include "config.hpp"
+
+NodeConfig::NodeConfig(String ssid, String password, uint16_t port = 555, bool isRoot = false, Scheduler *scheduler, uint8_t room_id, uint8_t led_pin, uint8_t led_count, HardwareSerial *serial, String version)
+{
+
+    // Mesh Config
+    this->mesh_config.ssid = ssid;
+    this->mesh_config.password = password;
+    this->mesh_config.containsRoot = true;
+    this->mesh_config.setRoot = isRoot;
+    this->mesh_config.scheduler = scheduler;
+    // Serial Config
+    this->serial_config.serial = serial;
+    // Room Config
+    this->room_config.id = room_id;
+    // Light Config
+    this->light_config.effect_speed = 150;
+    this->light_config.led_count = led_count;
+    this->light_config.led_pin = led_pin;
+}
--- esp-firmware/general-node/src/config.hpp
+++ esp-firmware/general-node/src/config.hpp
@@ -25,7 +25,6 @@
 #include <Arduino.h>
 #include <string.h>
 #include <painlessMesh.h>
-#include <NeoPixelBus.h>
 /*
  * Config related to painless mesh
  */
@@ -41,7 +40,7 @@ public:
     /*
      * The password of the mesh network
      */
-    String psk;
+    String password;
 
     /*
      * The port of the mesh network
@@ -67,13 +66,13 @@ class LightConfig
 {
 public:
     /*
-     * TBD To Be Defined Exactly
+     * To Be Defined Exactly
      */
     uint8_t effect_speed;
     /*
      * NeoPixel Pin number
      */
-    uint8_t pin;
+    uint8_t led_pin;
     /*
      * NeoPixel Pin number
      */
@@ -83,7 +82,7 @@ public:
 class SerialConfig
 {
 public:
-    HardwareSerial &serial;
+    HardwareSerial *serial;
 };
 
 /*
@@ -100,9 +99,9 @@ class NodeConfig
 {
 public:
     /*
-    * Version Number of the Program
-    * TODO: AutoGenerate
-    */
+     * Version Number of the Program
+     * TODO: AutoGenerate
+     */
     String version;
     /*
      * Room Config
@@ -124,4 +123,7 @@ public:
      * All user config variables pertaining to serial communication bit ( Baud Rate, etc)
      */
     SerialConfig serial_config;
+
+    // Constructor
+    NodeConfig(String ssid, String password, uint16_t port = 555, bool isRoot = false, Scheduler *scheduler, uint8_t room_id, uint8_t led_pin, uint8_t led_count, HardwareSerial *serial, String version);
 };

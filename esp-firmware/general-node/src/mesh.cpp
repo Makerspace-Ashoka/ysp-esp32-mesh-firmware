75ffc0e714b159bc5bc09e46b5992eda3c61b121
Sai Khurana <sai.khurana@ashoka.edu.in>
2024-06-12 10:58:00 +0530

    cleanup + update docs for mesh.cpp

--- esp-firmware/general-node/src/mesh.cpp
+++ esp-firmware/general-node/src/mesh.cpp
@@ -1,9 +1,5 @@
 #include "mesh.hpp"
 
-/**
- * @brief Start Painless Mesh
- *
- */
 void Mesh::init()
 {
 
@@ -14,21 +10,15 @@ void Mesh::init()
         this->mesh.setContainsRoot(true);
     }
 
-    // Set Node to Root only if it's needed
     if (this->config->setRoot)
     {
         this->mesh.setRoot(true);
     }
 
     this->mesh.init(this->config->ssid.c_str(), this->config->password.c_str(), this->config->scheduler);
-    // Update the ID in NodeConfig
     this->nodeConfig->setNodeId(this->mesh.getNodeId());
 }
 
-/**
- * @brief Subprocess
- *
- */
 void Mesh::loop()
 {
     this->mesh.update();
@@ -51,23 +41,11 @@ String Mesh::getTopology(bool pretty = false)
     return this->mesh.subConnectionJson(pretty);
 }
 
-/**
- * @brief
- *
- * @param callback
- */
 void Mesh::onReceive(void (*callback)(uint32_t from, String &msg))
 {
     this->mesh.onReceive(callback);
 }
 
-/**
- * @brief
- *
- * @param node
- * @param end_node
- * @return vector<uint32_t>
- */
 vector<uint32_t> Mesh::pathFinder(painlessmesh::protocol::NodeTree &node, uint32_t end_node)
 {
     if (node.nodeId == end_node)
@@ -89,12 +67,7 @@ vector<uint32_t> Mesh::pathFinder(painlessmesh::protocol::NodeTree &node, uint32
     }
     return {};
 }
-/**
- * @brief
- *
- * @param end_node
- * @return vector<uint32_t>
- */
+
 vector<uint32_t> Mesh::getPathToNode(uint32_t end_node)
 {
     painlessmesh::protocol::NodeTree node = this->mesh.asNodeTree();
@@ -109,11 +82,6 @@ uint32_t Mesh::getOwnNodeId()
 int Mesh::getNodesCount(){
     return this -> mesh.getNodeList().size();}
 
-/**
- * @brief Construct a new Mesh:: Mesh object
- *
- * @param node_config
- */
 Mesh::Mesh(NodeConfig &node_config)
 {
     this->config = &node_config.mesh_config;
